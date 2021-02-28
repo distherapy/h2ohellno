4eee65e18ddfbca0908d9188338dc5aaeebf2e0f
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-02-28 13:12:02 -0500

    Create basicneeds.py

---
+++ basicneeds.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import numpy as np
+import pandas as pd
+
+init_data = pd.read_csv("/home/sixie6e/exploitation/basics/mapping/wdi_least_sq_csv/basicneeds.csv/")
+plt.show()
